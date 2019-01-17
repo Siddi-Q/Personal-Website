ed651882e40a139dc52c672b50dfda192bb8355b
Saddique Shafi <saddiqueashafi@gmail.com>
2019-01-16 22:20:55 -0500

    initialzied node, modularized html using ejs

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + '/styles'));
+
+app.get("/", function(req, res){
+  res.render("home");
+});
+
+app.get("/home", function(req, res){
+  res.redirect("/");
+});
+
+app.get("/resume", function(req, res){
+  res.render("resume");
+});
+
+app.listen(port, process.env.IP, function(){
+  console.log(`sever started on ${port}!`);
+});
