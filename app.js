0d608a336bf01dfd985b565e944b6439c19770b6
Saddique Shafi <saddiqueashafi@gmail.com>
2019-01-23 21:47:16 -0500

    Added personalWebsite.js file and configured it and also made changes to the folder structure and made html titles dynamic

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 app.set("view engine", "ejs");
-app.use(express.static(__dirname + '/styles'));
+app.use(express.static(__dirname + '/assets/styles'));
+app.use(express.static(__dirname + '/assets/js'));
 
 app.get("/", function(req, res){
-  res.render("home");
+  res.render("home", {page: "Home"});
 });
 
 app.get("/home", function(req, res){
@@ -15,15 +16,15 @@ app.get("/home", function(req, res){
 });
 
 app.get("/resume", function(req, res){
-  res.render("resume");
+  res.render("resume", {page: "Resume"});
 });
 
 app.get("/about", function(req, res){
-  res.render("about");
+  res.render("about", {page: "About"});
 });
 
 app.get("/contact", function(req, res){
-  res.render("contact");
+  res.render("contact", {page: "Contact"});
 });
 
 app.listen(port, process.env.IP, function(){
