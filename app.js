aafdafdf9f21118ced6e678f7d8fd48e1f1804c6
Saddique Shafi <saddiqueashafi@gmail.com>
2019-06-23 20:48:52 -0400

    Added code to automatically determine the current year and display it in the footer of each page.

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const currentYear = require('./server/utils/year');
+
 const port = process.env.PORT || 3000;
 const app = express();
 
@@ -8,7 +10,7 @@ app.use(express.static(__dirname + '/assets/styles'));
 app.use(express.static(__dirname + '/assets/js'));
 
 app.get("/", function(req, res){
-  res.render("home", {page: "Home"});
+  res.render("home", {page: "Home", year: currentYear});
 });
 
 app.get("/home", function(req, res){
@@ -16,17 +18,17 @@ app.get("/home", function(req, res){
 });
 
 app.get("/resume", function(req, res){
-  res.render("resume", {page: "Resume"});
+  res.render("resume", {page: "Resume", year: currentYear});
 });
 
 app.get("/about", function(req, res){
-  res.render("about", {page: "About"});
+  res.render("about", {page: "About", year: currentYear});
 });
 
 app.get("/contact", function(req, res){
-  res.render("contact", {page: "Contact"});
+  res.render("contact", {page: "Contact", year: currentYear});
 });
 
 app.listen(port, process.env.IP, function(){
-  console.log(`sever started on ${port}!`);
+  console.log(`Sever started on port ${port}!`);
 });
---
+++ server/utils/year.js
@@ -0,0 +1,4 @@
+var date = new Date();
+var year = date.getFullYear();
+
+module.exports = year
