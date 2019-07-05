f3c078e0872d35d28fdc1bf40dafaee655c22951
Saddique Shafi <saddiqueashafi@gmail.com>
2019-07-04 21:24:58 -0400

    Added icons to resume and contact page, updated html and css in home page and added animation (in js) in home page.

--- assets/js/personalWebsite.js
+++ assets/js/personalWebsite.js
@@ -1,1 +1,28 @@
-console.log("Connected!")
+// Wrap every letter in a span
+$('.welcome').each(function(){
+  $(this)
+  .html(
+    $(this)
+    .text()
+    .replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>")
+  );
+});
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.welcome .letter',
+    scale: [4,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 950,
+    delay: function(el, i) {
+      return 70*i;
+    }
+  }).add({
+    targets: '.welcome',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
